4852b5e403fbd30c167441d760708bf5d81ffe40
Mark Harmsen <mharmsenr@gmail.com>
2025-08-11 22:38:54 -0500

    show edit and destroy

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+    before_action :set_task, only: %i[show edit update destroy]
     def index
         @tasks = Task.all
     end
@@ -7,18 +8,40 @@ class TasksController < ApplicationController
         @task = Task.new
     end
 
+    def show; end
+
     def create
         task = Task.new(task_params)
         if task.save
-            redirect_to root_path
+            redirect_to task
         else
             render :new, status: :unprocessable_entity
         end
+    end
+
+    def edit; end
+
+    def update
+        @task = Task.find(params[:id])
+        if @task.update(task_params)
+            redirect_to @task
+        else
+            render :edit, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @task.destroy
+        redirect_to tasks_path
     end
 
     private
 
     def task_params
         params.expect(task: :name)
+    end
+
+    def set_task
+        @task = Task.find params[:id]
     end
 end
