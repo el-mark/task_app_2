a1e97be31651c20a021414b3a96de2d17314a171
Mark Harmsen <mharmsenr@gmail.com>
2025-08-11 21:54:20 -0500

    add task, plus list of tasks

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,24 @@
+class TasksController < ApplicationController
+    def index
+        @tasks = Task.all
+    end
+
+    def new
+        @task = Task.new
+    end
+
+    def create
+        task = Task.new(task_params)
+        if task.save
+            redirect_to root_path
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def task_params
+        params.expect(task: :name)
+    end
+end
